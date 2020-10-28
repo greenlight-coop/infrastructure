32cadc3d8fd084c1b16f94b1cf1ea0fbb4361cfa
Eric <etavela@gmail.com>
2020-10-28 09:33:42 -0400

    (WIP) Incorporating GKE cluster - revised to use standard GCP GKE resources

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -12,7 +12,7 @@ resource "google_container_cluster" "primary" {
 }
 
 resource "google_container_node_pool" "primary_nodes" {
-  name               = 'primary-node-pool'
+  name               = "primary-node-pool"
   project            = module.project-factory.project_id
   location           = var.zone
   cluster            = google_container_cluster.primary.name
