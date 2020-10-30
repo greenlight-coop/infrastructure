be3f19f101bb9050c1f2a21541d9ad14a9fa2e7b
Eric <etavela@gmail.com>
2020-10-30 19:05:58 -0400

    Disabled DNS records

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,85 +74,105 @@ resource "google_project_service" "dns-development" {
   service = "dns.googleapis.com"
 }
 
-resource "google_dns_managed_zone" "root" {
-  name        = "greenlight-coop-zone"
-  dns_name    = "greenlight.com."
-  project     = google_project.network.project_id
-  description = "DNS for greenlight.coop"
-  depends_on  = [google_project_service.dns-network]
-}
+# resource "google_dns_managed_zone" "root" {
+#   name        = "greenlight-coop-zone"
+#   dns_name    = "greenlight.com."
+#   project     = google_project.network.project_id
+#   description = "DNS for greenlight.coop"
+#   depends_on  = [google_project_service.dns-network]
+# }
 
-resource "google_dns_record_set" "public_website_a" {
-  name         = "greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "A"
-  ttl          = 300
+# resource "google_dns_record_set" "public_website_a" {
+#   name         = "greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "A"
+#   ttl          = 300
 
-  rrdatas = ["68.168.102.9"]
-}
+#   rrdatas = ["68.168.102.9"]
+# }
 
-resource "google_dns_record_set" "www" {
-  name         = "www.greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "CNAME"
-  ttl          = 300
+# resource "google_dns_record_set" "www" {
+#   name         = "www.greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "CNAME"
+#   ttl          = 300
 
-  rrdatas = ["greenlight.com."]
-}
+#   rrdatas = ["greenlight.com."]
+# }
 
-resource "google_dns_record_set" "mx" {
-  name         = "greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "MX"
-  ttl          = 300
+# resource "google_dns_record_set" "mx" {
+#   name         = "greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "MX"
+#   ttl          = 300
 
-  rrdatas = [
-    "0 aspmx.l.google.com.",
-    "5 alt1.aspmx.l.google.com.",
-    "5 alt2.aspmx.l.google.com.",
-    "10 alt3.aspmx.l.google.com.",
-    "10 alt4.aspmx.l.google.com.",
-    "15 5r2no3qthkyojukke5exmz5xtrxwze6osdjunw77xwrs2ohwgpma.mx-verification.google.com.",
-  ]
-}
+#   rrdatas = [
+#     "0 aspmx.l.google.com.",
+#     "5 alt1.aspmx.l.google.com.",
+#     "5 alt2.aspmx.l.google.com.",
+#     "10 alt3.aspmx.l.google.com.",
+#     "10 alt4.aspmx.l.google.com.",
+#     "15 5r2no3qthkyojukke5exmz5xtrxwze6osdjunw77xwrs2ohwgpma.mx-verification.google.com.",
+#   ]
+# }
 
-resource "google_dns_record_set" "google-site-verification-txt" {
-  name         = "greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "TXT"
-  ttl          = 300
+# resource "google_dns_record_set" "google-site-verification-txt" {
+#   name         = "greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "TXT"
+#   ttl          = 300
 
-  rrdatas = ["google-site-verification=lDW-ce49Ygc1lgkankOG-pgx3shPmj_Az5i1df7rkw4"]
-}
+#   rrdatas = ["google-site-verification=lDW-ce49Ygc1lgkankOG-pgx3shPmj_Az5i1df7rkw4"]
+# }
 
-resource "google_dns_record_set" "github-verification-txt" {
-  name         = "_github-challenge-greenlight-coop.greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "TXT"
-  ttl          = 300
+# resource "google_dns_record_set" "github-verification-txt" {
+#   name         = "_github-challenge-greenlight-coop.greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "TXT"
+#   ttl          = 300
 
-  rrdatas = ["5effef20f4"]
-}
+#   rrdatas = ["5effef20f4"]
+# }
 
-resource "google_dns_managed_zone" "dev" {
-  name        = "dev-greenlight-coop-zone"
-  dns_name    = "dev.greenlight.com."
-  project     = google_project.development.project_id
-  description = "DNS for dev.greenlight.coop"
-  depends_on  = [google_project_service.dns-development]
-}
+# resource "google_dns_managed_zone" "dev" {
+#   name        = "dev-greenlight-coop-zone"
+#   dns_name    = "dev.greenlight.com."
+#   project     = google_project.development.project_id
+#   description = "DNS for dev.greenlight.coop"
+#   depends_on  = [google_project_service.dns-development]
+# }
 
-resource "google_dns_record_set" "dev_name_servers" {
-  name         = "dev.greenlight.com."
-  project      = google_project.network.project_id
-  managed_zone = google_dns_managed_zone.root.name
-  type         = "NS"
-  ttl          = 300
+# resource "google_dns_record_set" "dev_name_servers" {
+#   name         = "dev.greenlight.com."
+#   project      = google_project.network.project_id
+#   managed_zone = google_dns_managed_zone.root.name
+#   type         = "NS"
+#   ttl          = 300
 
-  rrdatas = google_dns_managed_zone.dev.name_servers
-}
+#   rrdatas = google_dns_managed_zone.dev.name_servers
+# }
+
+# resource "google_dns_record_set" "greenlight-development-cluster-ingress-a-test" {
+#   name         = "test.dev.greenlight.com."
+#   project      = google_project.development.project_id
+#   managed_zone = google_dns_managed_zone.dev.name
+#   type         = "A"
+#   ttl          = 300
+
+#   rrdatas = ["34.86.70.60"]
+# }
+
+# resource "google_dns_record_set" "greenlight-development-cluster-ingress-a-wildcard" {
+#   name         = "*.dev.greenlight.com."
+#   project      = google_project.development.project_id
+#   managed_zone = google_dns_managed_zone.dev.name
+#   type         = "A"
+#   ttl          = 300
+
+#   rrdatas = ["34.86.70.60"]
+# }
--- terraform/output.tf
+++ terraform/output.tf
@@ -6,10 +6,10 @@ output "network_project_id" {
   value = google_project.network.project_id
 }
 
-output "root_name_servers" {
-  value = google_dns_managed_zone.root.name_servers
-}
+# output "root_name_servers" {
+#   value = google_dns_managed_zone.root.name_servers
+# }
 
-output "dev_name_servers" {
-  value = google_dns_managed_zone.dev.name_servers
-}
+# output "dev_name_servers" {
+#   value = google_dns_managed_zone.dev.name_servers
+# }
