8d0111172ac5d53f0630372f069f46679d8bbdad
Eric <etavela@gmail.com>
2020-10-31 12:50:29 -0400

    (WIP) Incorporating Argo CD

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -65,6 +65,12 @@ resource "helm_release" "ingress-nginx" {
   repository  = "https://kubernetes.github.io/ingress-nginx"
   chart       = "ingress-nginx"
   version     = "3.7.1"
+
+  set {
+    name  = "controller.enableTLSPassthrough"
+    value = "true"
+  }
+
   depends_on = [
     null_resource.kubeconfig
   ]
