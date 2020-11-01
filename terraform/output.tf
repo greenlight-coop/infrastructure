e80099dde2ff44fdbdc676c8c79d3fd21511f5b2
Eric <etavela@gmail.com>
2020-11-01 18:52:36 -0500

    Revised output

--- terraform/output.tf
+++ terraform/output.tf
@@ -6,6 +6,10 @@ output "network_project_id" {
   value = google_project.network.project_id
 }
 
+output "cluster_endpoint" {
+  value = google_container_cluster.development.endpoint
+}
+
 output "kubeconfig_command" {
   value ="gcloud container clusters get-credentials ${google_container_cluster.development.name} --project ${google_project.development.project_id} --zone ${var.zone}"
 }
