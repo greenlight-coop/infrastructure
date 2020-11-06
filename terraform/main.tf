84596765e8be76b43c746c78425a04aad5b4fc5d
Eric <etavela@gmail.com>
2020-11-06 18:06:18 -0500

    Incorporating Knative

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,6 +22,10 @@ terraform {
       source =  "hashicorp/random"
       version = "~> 3.0.0"
     }
+    null = {
+      source =  "hashicorp/null"
+      version = "~> 3.0.0"
+    }
   }
 
   backend "gcs" {
