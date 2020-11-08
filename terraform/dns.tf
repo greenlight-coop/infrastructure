b93c69db116315dffd9e176abc2e75d9a3e2ea90
Eric <etavela@gmail.com>
2020-11-08 09:25:37 -0500

    Updated README and revised feature subdomain names

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -4,11 +4,11 @@ resource "google_project_service" "dns-development" {
 }
 
 locals {
-  domain_name_prefix  = terraform.workspace == "default" ? "" : "${terraform.workspace}-"
-  ingress_domain_name             = "${local.domain_name_prefix}ingress.greenlightcoop.dev"
-  knative_domain_name             = "${local.domain_name_prefix}knative.greenlightcoop.dev"
-  apps_domain_name                = "${local.domain_name_prefix}apps.greenlightcoop.dev"
-  api_domain_name                 = "${local.domain_name_prefix}api.greenlightcoop.dev"
+  domain_name_suffix              = terraform.workspace == "default" ? "" : "-${terraform.workspace}"
+  ingress_domain_name             = "ingress${local.domain_name_suffix}.greenlightcoop.dev"
+  knative_domain_name             = "knative${local.domain_name_suffix}.greenlightcoop.dev"
+  apps_domain_name                = "apps${local.domain_name_suffix}.greenlightcoop.dev"
+  api_domain_name                 = "api${local.domain_name_suffix}.greenlightcoop.dev"
   ingress_domain_name_terminated  = "${local.ingress_domain_name}."
   knative_domain_name_terminated  = "${local.knative_domain_name}."
   apps_domain_name_terminated     = "${local.apps_domain_name}."
