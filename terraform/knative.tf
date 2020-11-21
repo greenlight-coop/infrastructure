f09645e1cdaeb1462f4d4ae2a5885586c52b3e9f
Eric <etavela@gmail.com>
2020-11-21 13:39:10 -0500

    (WIP) Refactoring Terraform build and shifting resources to Argo CD

--- terraform/knative.tf
+++ terraform/knative.tf
@@ -1,133 +1,133 @@
-# # Downloaded from https://github.com/knative/serving/releases/download/v0.18.0/serving-crds.yaml
-# resource "null_resource" "knative-serving-crds" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply --filename manifests/knative-serving-crds.yaml"
-#   }
-#   depends_on = [
-#     helm_release.ingress-nginx,
-#     helm_release.cert-manager
-#   ]
-# }
+# Downloaded from https://github.com/knative/serving/releases/download/v0.18.0/serving-crds.yaml
+resource "null_resource" "knative-serving-crds" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename manifests/knative-serving-crds.yaml"
+  }
+  depends_on = [
+    helm_release.ingress-nginx,
+    helm_release.cert-manager
+  ]
+}
 
-# # Downloaded from https://github.com/knative/serving/releases/download/v0.18.0/serving-core.yaml
-# resource "null_resource" "knative-serving-core" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply --filename manifests/knative-serving-core.yaml"
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-crds
-#   ]
-# }
+# Downloaded from https://github.com/knative/serving/releases/download/v0.18.0/serving-core.yaml
+resource "null_resource" "knative-serving-core" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename manifests/knative-serving-core.yaml"
+  }
+  depends_on = [
+    null_resource.knative-serving-crds
+  ]
+}
 
-# resource "null_resource" "istioctl-install" {
-#   provisioner "local-exec" {
-#     command = "istioctl install --skip-confirmation"
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-core,
-#   ]
-# }
+resource "null_resource" "istioctl-install" {
+  provisioner "local-exec" {
+    command = "istioctl install --skip-confirmation"
+  }
+  depends_on = [
+    null_resource.knative-serving-core,
+  ]
+}
 
-# resource "null_resource" "istio-minimal-operator" {
-#   provisioner "local-exec" {
-#     command = "istioctl manifest install -f manifests/istio-minimal-operator.yaml"
-#   }
-#   depends_on = [
-#     null_resource.istioctl-install
-#   ]
-# }
+resource "null_resource" "istio-minimal-operator" {
+  provisioner "local-exec" {
+    command = "istioctl manifest install -f manifests/istio-minimal-operator.yaml"
+  }
+  depends_on = [
+    null_resource.istioctl-install
+  ]
+}
 
-# data "kubernetes_service" "istio-ingressgateway" {
-#   metadata {
-#     namespace = "istio-system"
-#     name      = "istio-ingressgateway"
-#   }
-#   depends_on = [
-#     null_resource.istio-minimal-operator
-#   ]
-# }
+data "kubernetes_service" "istio-ingressgateway" {
+  metadata {
+    namespace = "istio-system"
+    name      = "istio-ingressgateway"
+  }
+  depends_on = [
+    null_resource.istio-minimal-operator
+  ]
+}
 
-# resource "null_resource" "enable-serving-istio-injection" {
-#   provisioner "local-exec" {
-#     command = "kubectl label namespace knative-serving istio-injection=enabled"
-#   }
-#   depends_on = [
-#     null_resource.istio-minimal-operator,
-#   ]
-# }
+resource "null_resource" "enable-serving-istio-injection" {
+  provisioner "local-exec" {
+    command = "kubectl label namespace knative-serving istio-injection=enabled"
+  }
+  depends_on = [
+    null_resource.istio-minimal-operator,
+  ]
+}
 
-# resource "k8s_manifest" "knative-serving-permissive" {
-#   content = <<-EOT
-#     apiVersion: "security.istio.io/v1beta1"
-#     kind: "PeerAuthentication"
-#     metadata:
-#       name: "default"
-#       namespace: "knative-serving"
-#     spec:
-#       mtls:
-#         mode: PERMISSIVE
-#   EOT
-#   depends_on = [
-#     null_resource.enable-serving-istio-injection
-#   ]
-# }
+resource "k8s_manifest" "knative-serving-permissive" {
+  content = <<-EOT
+    apiVersion: "security.istio.io/v1beta1"
+    kind: "PeerAuthentication"
+    metadata:
+      name: "default"
+      namespace: "knative-serving"
+    spec:
+      mtls:
+        mode: PERMISSIVE
+  EOT
+  depends_on = [
+    null_resource.enable-serving-istio-injection
+  ]
+}
 
-# # Downloaded from https://github.com/knative/net-istio/releases/download/v0.18.0/release.yaml
-# resource "null_resource" "knative-serving-istio" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply --filename manifests/knative-serving-istio.yaml"
-#   }
-#   depends_on = [
-#     k8s_manifest.knative-serving-permissive
-#   ]
-# }
+# Downloaded from https://github.com/knative/net-istio/releases/download/v0.18.0/release.yaml
+resource "null_resource" "knative-serving-istio" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename manifests/knative-serving-istio.yaml"
+  }
+  depends_on = [
+    k8s_manifest.knative-serving-permissive
+  ]
+}
 
-# resource "null_resource" "knative-serving-config-domain" {
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       kubectl patch configmap/config-domain \
-#         --namespace knative-serving \
-#         --type merge \
-#         --patch '{"data":{"${local.knative_domain_name}":""}}'
-#     EOT
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-istio,
-#     null_resource.knative-serving-core,
-#     google_dns_record_set.wildcard-knative-greenlightcoop-dev-a-record
-#   ]
-# }
+resource "null_resource" "knative-serving-config-domain" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      kubectl patch configmap/config-domain \
+        --namespace knative-serving \
+        --type merge \
+        --patch '{"data":{"${local.knative_domain_name}":""}}'
+    EOT
+  }
+  depends_on = [
+    null_resource.knative-serving-istio,
+    null_resource.knative-serving-core,
+    google_dns_record_set.wildcard-knative-greenlightcoop-dev-a-record
+  ]
+}
 
-# # Downloaded from https://github.com/knative/net-certmanager/releases/download/v0.18.0/release.yaml
-# resource "null_resource" "knative-serving-certmanager-extension" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply --filename manifests/knative-serving-certmanager-extension.yaml"
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-config-domain
-#   ]
-# }
+# Downloaded from https://github.com/knative/net-certmanager/releases/download/v0.18.0/release.yaml
+resource "null_resource" "knative-serving-certmanager-extension" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename manifests/knative-serving-certmanager-extension.yaml"
+  }
+  depends_on = [
+    null_resource.knative-serving-config-domain
+  ]
+}
 
-# # Edited to include installed letsencrypt ClusterIssuer configuration per instructions at
-# # https://knative.dev/docs/serving/using-auto-tls/#configure-config-certmanager-configmap
-# resource "null_resource" "knative-serving-certmanager-extension-issuer" {
-#   provisioner "local-exec" {
-#     command = <<-EOT
-#       cat <<EOF | kubectl apply -f -
-#       ${templatefile("manifests/knative-serving-certmanager-extension-issuer.yaml", {tls_cert_issuer = local.tls_cert_issuer})}
-#       EOF
-#     EOT
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-certmanager-extension
-#   ]
-# }
+# Edited to include installed letsencrypt ClusterIssuer configuration per instructions at
+# https://knative.dev/docs/serving/using-auto-tls/#configure-config-certmanager-configmap
+resource "null_resource" "knative-serving-certmanager-extension-issuer" {
+  provisioner "local-exec" {
+    command = <<-EOT
+      cat <<EOF | kubectl apply -f -
+      ${templatefile("manifests/knative-serving-certmanager-extension-issuer.yaml", {tls_cert_issuer = local.tls_cert_issuer})}
+      EOF
+    EOT
+  }
+  depends_on = [
+    null_resource.knative-serving-certmanager-extension
+  ]
+}
 
-# resource "null_resource" "knative-serving-config-network-tls" {
-#   provisioner "local-exec" {
-#     command = "kubectl apply --filename manifests/knative-serving-config-network-tls.yaml"
-#   }
-#   depends_on = [
-#     null_resource.knative-serving-certmanager-extension-issuer
-#   ]
-# }
+resource "null_resource" "knative-serving-config-network-tls" {
+  provisioner "local-exec" {
+    command = "kubectl apply --filename manifests/knative-serving-config-network-tls.yaml"
+  }
+  depends_on = [
+    null_resource.knative-serving-certmanager-extension-issuer
+  ]
+}
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,29 +1,54 @@
 # Argo CD
 
-resource "k8s_manifest" "argocd-github-ssh-key-secret" {
-  content = templatefile("manifests/argocd-github-ssh-key-secret.yaml", {
-    bot_private_key = local.bot_private_key
-  })
+resource "kubernetes_secret" "argocd-github-ssh-key-secret" {
+  metadata {
+    name = "github-ssh-key"
+    namespace = "argocd"
+  }
+
+  data = {
+    sshPrivateKey = <<SSH
+${local.bot_private_key}
+    SSH
+  }
+
+  depends_on = [
+    google_container_node_pool.development_primary_nodes
+  ]
+}
+
+resource "kubernetes_secret" "default-admin-password-secret" {
+  metadata {
+    name = "admin-password-secret"
+    namespace = "default"
+  }
+
+  data = {
+    password: ${local.admin_password}
+  }
+
   depends_on = [
     kubernetes_namespace.argocd
   ]
 }
 
-resource "k8s_manifest" "default-admin-password-secret" {
-  content = templatefile("manifests/admin-password-secret.yaml", {
-    namespace       = "default"
-    admin_password  = local.admin_password
-  })
-  depends_on = [google_container_node_pool.development_primary_nodes]
-}
-
 # Grafana
 
-resource "k8s_manifest" "grafana-datasources-secret" {
-  content = templatefile("manifests/grafana-datasources-secret.yaml", {
+resource "kubernetes_secret" "grafana-datasources-secret" {
+  metadata {
+    name = "grafana-datasources"
     namespace = "default"
-  })
-  depends_on = [google_container_node_pool.development_primary_nodes]
+  }
+
+  data = {
+    datasource.yaml = <<DATASOURCES
+${file("manifests/grafana-datasources.yaml")}
+    DATASOURCES
+  }
+
+  depends_on = [
+    google_container_node_pool.development_primary_nodes
+  ]
 }
 
 # greenlight-pipelines
@@ -41,9 +66,9 @@ resource "kubernetes_secret" "greenlight-pipelines-git-auth" {
     known_hosts = <<KNOWN_HOSTS
 github.com,192.30.253.112 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
     KNOWN_HOSTS
-    ssh-privatekey = <<DOCKER
+    ssh-privatekey = <<SSH
 ${local.bot_private_key}
-    DOCKER
+    SSH
   }
   type = "kubernetes.io/ssh-auth"
 
