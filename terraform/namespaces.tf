86a0ff8acc3145d84544b0b3d6dfe6f515a2a6d4
Eric <etavela@gmail.com>
2020-11-22 14:42:05 -0500

    (WIP) Refactoring Terraform build and shifting resources to Argo CD

--- terraform/namespaces.tf
+++ terraform/namespaces.tf
@@ -28,10 +28,11 @@ resource "kubernetes_namespace" "greenlight-pipelines" {
 resource "kubernetes_namespace" "knative-serving" {
   metadata {
     name = "knative-serving"
+    labels = {
+      istio-injection = "enabled"
+    }
   }
-  labels {
-    istio-injection = "enabled"
-  }
+
   depends_on = [
     google_container_node_pool.development_primary_nodes
   ]
