a615b7b79762d67c1794e0ca51b695b1933b83e8
Eric <etavela@gmail.com>
2020-11-24 17:10:16 -0500

    Adding preview deployment and other pull-request build behavior

--- terraform/dns.tf
+++ terraform/dns.tf
@@ -6,7 +6,7 @@ resource "google_project_service" "dns-development" {
 locals {
   domain_name_suffix              = terraform.workspace == "default" ? "" : "-${terraform.workspace}"
   ingress_domain_name             = "ingress${local.domain_name_suffix}.greenlightcoop.dev"
-  knative_domain_name             = "knative${local.domain_name_suffix}.greenlightcoop.dev"
+  knative_domain_name             = "kn${local.domain_name_suffix}.greenlightcoop.dev"
   apps_domain_name                = "apps${local.domain_name_suffix}.greenlightcoop.dev"
   api_domain_name                 = "api${local.domain_name_suffix}.greenlightcoop.dev"
   ingress_domain_name_terminated  = "${local.ingress_domain_name}."
