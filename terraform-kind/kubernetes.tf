18b60bb960021695baaa2fe3f0efc032263cfe03
Eric <etavela@gmail.com>
2020-11-28 06:30:30 -0500

    (WIP) Setting up kind cluster

--- terraform-kind/kubernetes.tf
+++ terraform-kind/kubernetes.tf
@@ -11,9 +11,6 @@ resource "null_resource" "greenlight-kind" {
   provisioner "local-exec" {
     command = "kind create cluster --name greenlight --config manifests/kind-config.yaml"
   }
-}
-
-resource "null_resource" "destroy-greenlight-kind" {
   provisioner "local-exec" {
     when    = destroy
     command = "kind delete cluster --name greenlight"
