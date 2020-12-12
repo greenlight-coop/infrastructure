80ad9e24310d6dd12564d7945bbe07c76d677d14
Eric <etavela@gmail.com>
2020-12-12 15:48:32 -0500

    Revising to move k8s configuration into Argo CD

--- terraform/argocd.tf
+++ terraform/argocd.tf
@@ -63,6 +63,29 @@ resource "helm_release" "argo-cd" {
               namespace: knative-serving-admin
               jsonPointers:
               - /rules
+          cert-manager.io/ClusterIssuer:
+            health.lua: |
+              hs = {}
+              if obj.status ~= nil then
+                if obj.status.conditions ~= nil then
+                  for i, condition in ipairs(obj.status.conditions) do
+                    if condition.type == "Ready" and condition.status == "False" then
+                      hs.status = "Degraded"
+                      hs.message = condition.message
+                      return hs
+                    end
+                    if condition.type == "Ready" and condition.status == "True" then
+                      hs.status = "Healthy"
+                      hs.message = condition.message
+                      return hs
+                    end
+                  end
+                end
+              end
+
+              hs.status = "Progressing"
+              hs.message = "Initializing issuer"
+              return hs
     configs:
       secret:
         githubSecret: ${local.webhook_secret}
