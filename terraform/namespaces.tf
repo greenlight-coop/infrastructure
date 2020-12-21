07465e4dc3e8e4107745ca19ea54402b242207ed
Eric <etavela@gmail.com>
2020-12-21 15:18:24 -0500

    (WIP) Implementing test stage pipeline

--- terraform/namespaces.tf
+++ terraform/namespaces.tf
@@ -13,9 +13,6 @@ resource "kubernetes_namespace" "argocd" {
 resource "kubernetes_namespace" "greenlight-pipelines" {
   metadata {
     name = "greenlight-pipelines"
-    labels = {
-      "webhooks.knative.dev/exclude" = "true"
-    }
   }
   depends_on = [
     google_container_node_pool.development_primary_nodes
