d847294c55738e4df9b47bb5a63d134a4abfa8b5
Eric <etavela@gmail.com>
2020-12-27 09:40:01 -0500

    Revised instructions and scripts for meta

--- terraform/teardown-workspace.sh
+++ terraform/teardown-workspace.sh
@@ -9,40 +9,15 @@ set -x
 CWD=$(pwd)
 GL_TERRAFORM=$CWD
 GL_GIT_BASE=$(cd ../../..; pwd)
-GL_DEVOPS=$GL_GIT_BASE/devops
-GL_INFRA=$GL_DEVOPS/infrastructure
-GL_ARGOCD_INFRA=$GL_DEVOPS/argocd-greenlight-infrastructure
-GL_STAGES=$GL_GIT_BASE/stages
-GL_STAGE_TEST=$GL_STAGES/greenlight-stage-test
-GL_SOFTWARE=$GL_GIT_BASE/software
-GL_HELM_CHARTS=$GL_SOFTWARE/greenlight-helm-charts
 
 TF_WORKSPACE=$(terraform workspace show)
 BRANCH=$(echo $TF_WORKSPACE | tr '-' '/')
 
-cd $GL_HELM_CHARTS
-git checkout main
-git pull
-git branch -d $BRANCH
-git push origin --delete $BRANCH
-
-cd $GL_STAGE_TEST
-git checkout main
-git pull
-git branch -d $BRANCH
-git push origin --delete $BRANCH
-
-cd $GL_ARGOCD_INFRA
-git checkout main
-git pull
-git branch -d $BRANCH
-git push origin --delete $BRANCH
-
-cd $GL_TERRAFORM
-git checkout master
-git pull
-git branch -d $BRANCH
-git push origin --delete $BRANCH
-
 terraform workspace select default
 terraform workspace delete $TF_WORKSPACE
+
+cd $GL_GIT_BASE
+meta git checkout main
+meta git pull
+meta git branch -d $BRANCH
+meta git push origin --delete $BRANCH
