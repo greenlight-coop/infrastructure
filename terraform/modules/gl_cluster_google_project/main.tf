cfa2d488d618441f8fc948ffc04ac0c228f9b897
Eric <etavela@gmail.com>
2021-03-07 15:27:16 -0500

    Adding functionality to create GCP project'

--- terraform/modules/gl_cluster_google_project/main.tf
+++ terraform/modules/gl_cluster_google_project/main.tf
@@ -20,8 +20,11 @@ provider "google" {
 resource "null_resource" "print-configuration" {
   provisioner "local-exec" {
     command = <<EOF
-    echo var.existing_project: ${var.existing_project} && \
-      echo var.project_name: ${var.project_name}
+      echo var.existing_project:    ${var.existing_project} && \
+      echo var.project_name:        ${var.project_name} && \
+      echo var.project_id:          ${var.project_id} && \
+      echo var.org_id:              ${var.org_id} && \
+      echo var.billing_account_id:  ${var.billing_account_id} && \
     EOF
   }
 }
