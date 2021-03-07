9074af4347dd11bb1f4c0582e8e11cc4fcd44cc0
Eric <etavela@gmail.com>
2021-03-07 17:18:50 -0500

    Revising configuration

--- terraform/greenlight_main/main.tf
+++ terraform/greenlight_main/main.tf
@@ -11,8 +11,8 @@ terraform {
 locals {
   default_project_id    = terraform.workspace == "default" ? "greenlight-coop-development" : "gl-development-feature-current"
   default_project_name  = terraform.workspace == "default" ? "greenlight-coop-development" : "gl-development-${terraform.workspace}"
-  project_id            = var.project_id == "" ? default_project_id : var.project_id
-  project_name          = var.project_name == "" ? default_project_name : var.project_name
+  project_id            = var.project_id == "" ? local.default_project_id : var.project_id
+  project_name          = var.project_name == "" ? local.default_project_name : var.project_name
   subdomain_suffix      = terraform.workspace == "default" ? "" : "-${terraform.workspace}"
   domain_name           = "apps${local.subdomain_suffix}.greenlightcoop.dev"
 }
