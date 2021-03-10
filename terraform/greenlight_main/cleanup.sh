7ed8987bd69fedfdcbdf1a12915c23a3df28cedd
Eric <etavela@gmail.com>
2021-03-09 19:33:10 -0500

    Configuring Argo CD and refactoring

--- terraform/greenlight_main/cleanup.sh
+++ terraform/greenlight_main/cleanup.sh
@@ -10,12 +10,24 @@ gcloud --project=$GCP_PROJECT_ID --quiet dns record-sets import -z $DNS_ZONE_NAM
 gcloud --project=$GCP_PROJECT_ID --quiet dns managed-zones delete $DNS_ZONE_NAME
 gcloud --project=$GCP_PROJECT_ID --quiet iam service-accounts delete dns-admin@$GCP_PROJECT_ID.iam.gserviceaccount.com
 gcloud --project=$GCP_PROJECT_ID --quiet container clusters delete greenlight-development-cluster
+
+# TBD
+# See https://github.com/pantheon-systems/kube-gce-cleanup
 # * Delete Load Balancers
 # * Check that all External IP Addresses are deleted (delete if necessary)
 # * Delete k8s_* Firewall Rules
 # * Delete Service Accounts
-# * Delete all Compute Engine Disks
 # * Delete all Compute Engine Instance Group Health Checks
+
+# * Delete all unused Compute Engine Disks
+for orphaned_disk_uri in $(gcloud compute disks list --uri --filter="-users:*" 2> /dev/null); do
+  orphaned_disk_name=${orphaned_disk_uri##*/}
+  orphaned_disk_zone_uri=${orphaned_disk_uri/\/disks\/${orphaned_disk_name}/}
+  orphaned_disk_zone=${orphaned_disk_zone_uri##*/}
+  if [ -n "${orphaned_disk_name}" ] && [ -n "${orphaned_disk_zone}" ] && gcloud compute disks delete ${orphaned_disk_name} --zone ${orphaned_disk_zone} --quiet; then
+    echo "deleted: ${orphaned_disk_zone}/${orphaned_disk_name}"
+  fi
+done
 
 terraform workspace select default
 terraform workspace delete -force $TF_WORKSPACE
