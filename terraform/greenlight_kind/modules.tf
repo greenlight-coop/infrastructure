71b5293be56317b1d99442f9bf7f70a337ebcaf1
Eric <etavela@gmail.com>
2021-03-09 20:44:14 -0500

    Configuring kind cluster and refactoring

--- terraform/greenlight_kind/modules.tf
+++ terraform/greenlight_kind/modules.tf
@@ -1,16 +1,16 @@
 module "kind_cluster" {
   source = "../modules/kind_cluster"
 
-  kind_tls_crt         = var.kind_tls_crt
-  kind_tls_key         = var.kind_tls_key
+  kind_tls_crt  = var.kind_tls_crt
+  kind_tls_key  = var.kind_tls_key
 }
 
 module "argo_cd" {
   source = "../modules/argo_cd"
 
-  admin_password    = local.admin_password
-  webhook_secret    = var.webhook_secret
-  domain_name       = local.domain_name
+  admin_password  = local.admin_password
+  webhook_secret  = var.webhook_secret
+  domain_name     = local.domain_name
 
   depends_on = [
     null_resource.update-kubeconfig,
