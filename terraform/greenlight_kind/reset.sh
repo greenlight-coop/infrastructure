92872a51eb5df7e374030b1bff1b75d30154e74a
Eric <etavela@gmail.com>
2021-03-10 20:33:50 -0500

    Refactoring base cluster configuration

--- terraform/greenlight_kind/reset.sh
+++ terraform/greenlight_kind/reset.sh
@@ -5,5 +5,5 @@ set -x
 TF_WORKSPACE=$(terraform workspace show)
 
 kind delete cluster --name greenlight
-rm -rf .terraform terraform.tf.state.d .terraform.lock.hcl
+rm -rf .terraform terraform.tfstate.d .terraform.lock.hcl
 terraform workspace new $TF_WORKSPACE
