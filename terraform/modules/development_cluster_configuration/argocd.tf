e0c264d9940f430f0c60b916bfd90014c23f67c0
Eric <etavela@gmail.com>
2021-03-11 04:41:42 -0500

    Refactoring development cluster configuration

--- terraform/modules/development_cluster_configuration/argocd.tf
+++ terraform/modules/development_cluster_configuration/argocd.tf
@@ -2,6 +2,7 @@ resource "null_resource" "base-application" {
   provisioner "local-exec" {
     command = "kubectl apply -n argocd -f -<<EOF\n${templatefile(
       "${path.module}/manifests/development-application.yaml", {
+        destination_server      = var.destination_server,
         domain_name             = var.domain_name,
         repo_url                = var.repo_url,
         target_revision         = var.target_revision
