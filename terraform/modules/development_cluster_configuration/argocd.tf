5e2dbea86071cc0d8b2608b280f8c054757b4af1
Eric <etavela@gmail.com>
2021-03-11 04:20:22 -0500

    Refactoring development cluster configuration

--- terraform/modules/development_cluster_configuration/argocd.tf
+++ terraform/modules/development_cluster_configuration/argocd.tf
@@ -1,31 +1,11 @@
-# resource "k8s_manifest" "argocd-greenlight-infrastructure-application" {
-#   content = templatefile(
-#     "${path.module}/manifests/argocd-greenlight-infrastructure-application.yaml", 
-#     {
-#       target_revision     = local.argocd_source_target_revision
-#       use_staging_certs   = var.use_staging_certs
-#       admin_email         = var.admin_email
-#       workspace_suffix    = var.workspace_suffix
-#       apps_domain_name    = var.apps_domain_name
-#       google_project_id   = var.project_id
-#       is_kind_cluster     = var.is_kind_cluster
-#       lightweight         = var.lightweight
-#     }
-#   )
-#   depends_on = [
-#     null_resource.argocd,
-#     k8s_manifest.argocd-project,
-#     kubernetes_secret.grafana-admin-password-secret,
-#     kubernetes_secret.greenlight-pipelines-git-auth,
-#     kubernetes_secret.greenlight-pipelines-docker-registry-credentials,
-#     kubernetes_secret.greenlight-pipelines-bot-github-token,
-#     kubernetes_secret.greenlight-pipelines-webhook-secret,
-#     kubernetes_secret.greenlight-pipelines-buildkit-client-certs,
-#     kubernetes_secret.greenlight-pipelines-buildkit-daemon-certs,
-#     kubernetes_namespace.greenlight-pipelines,
-#     kubernetes_namespace.knative-serving,
-#   ]
-#   timeouts {
-#     delete = "20m"
-#   }
-# }
+resource "null_resource" "base-application" {
+  provisioner "local-exec" {
+    command = "kubectl apply -n argocd -f -<<EOF\n${templatefile(
+      "${path.module}/manifests/development-application.yaml", {
+        domain_name             = var.domain_name,
+        repo_url                = var.repo_url,
+        target_revision         = var.target_revision
+      }
+    )}\nEOF"
+  }
+}
