c2cae946509990aa328acde62fd3a51ca317d9d0
Eric <etavela@gmail.com>
2021-03-14 17:15:17 -0400

    Setting up Jus Cogens infrastructure and revising module requirements

--- terraform/clients/jus_cogens/setup.sh
+++ terraform/clients/jus_cogens/setup.sh
@@ -5,6 +5,6 @@ set -ex
 terraform init
 
 # Create client kind cluster
-terraform apply -auto-approve -target=module.google_project.null_resource.update-kubeconfig
+terraform apply -auto-approve -target=module.google_project -target=null_resource.update-kubeconfig
 
 # Configure client cluster
