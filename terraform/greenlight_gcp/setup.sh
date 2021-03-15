082aa8b8ac2f64e2d4310cccfb7f5fe59ea2aa49
Eric <etavela@gmail.com>
2021-03-15 18:06:12 -0400

    Refactoring cluster creation to support multicluster approach

--- terraform/greenlight_gcp/setup.sh
+++ terraform/greenlight_gcp/setup.sh
@@ -11,12 +11,8 @@ terraform apply -auto-approve -target=module.google_project -target=null_resourc
 terraform apply -auto-approve -target=module.argo_cd
 kubectl -n argocd wait deployments -l app.kubernetes.io/part-of=argocd --for=condition=Available --timeout=240s
 
-# Install k8ssandra
-terraform apply -auto-approve -target=module.k8ssandra
-kubectl wait pods/k8ssandra-dc1-default-sts-0 --for=condition=Ready --timeout=600s
-
 # Install base cluster infrastructure
-terraform apply -auto-approve -target=module.base_cluster_configuration
+terraform apply -auto-approve -target=module.project_cluster
 sleep 120
 kubectl -n istio-system wait deployments/istiod --for=condition=Available --timeout=600s
 sleep 30
