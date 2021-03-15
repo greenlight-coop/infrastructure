860461cdc24e3e28539009a8333e4b9887386d14
Eric <etavela@gmail.com>
2021-03-15 19:17:42 -0400

    Refactoring cluster creation to support multicluster approach

--- terraform/modules/development_cluster_configuration/main.tf
+++ terraform/modules/development_cluster_configuration/main.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 0.14.8"
+
+  required_providers {
+    kubernetes = {
+      source =  "hashicorp/kubernetes"
+      version = "~> 2.0.2"
+    }
+    k8s = {
+      version = ">= 0.9.0"
+      source  = "banzaicloud/k8s"
+    }
+  }
 }
