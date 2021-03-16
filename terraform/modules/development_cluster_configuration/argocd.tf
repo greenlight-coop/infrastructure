2b400682d71984fa91d111c865e53823aee86e9a
Eric <etavela@gmail.com>
2021-03-16 11:28:01 -0400

    Refactoring cluster creation to support multicluster approach

--- terraform/modules/development_cluster_configuration/argocd.tf
+++ terraform/modules/development_cluster_configuration/argocd.tf
@@ -1,7 +1,6 @@
 resource "k8s_manifest" "development-application" {
   content = templatefile(
     "${path.module}/manifests/development-application.yaml", {
-      destination_server      = var.destination_server,
       domain_name             = var.domain_name,
       repo_url                = var.repo_url,
       target_revision         = var.target_revision
