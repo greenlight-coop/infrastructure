b9c96b3f5a824e8513daa23ac72d811c336bb4f4
Eric <etavela@gmail.com>
2021-09-06 17:28:44 -0400

    Upgraded Argo CD from 2.0.3 to 2.1.2

---
+++ terraform/modules/argo_cd/secrets.tf
@@ -0,0 +1,20 @@
+resource "kubernetes_secret" "argocd-github-ssh-key-secret" {
+  metadata {
+    name = "argoproj-ssh-creds"
+    namespace = "argocd"
+    labels = {
+      "argocd.argoproj.io/secret-type" = "repo-creds"
+    }
+  }
+
+  data = {
+    url = git@github.com
+    sshPrivateKey = <<SSH
+${var.bot_private_key}
+    SSH
+  }
+
+  depends_on = [
+    kubernetes_namespace.argocd
+  ]
+}
