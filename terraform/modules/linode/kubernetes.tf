47df8d811d5df0e6d59ac768b1813da0c899741b
Eric <etavela@gmail.com>
2021-10-31 06:14:47 -0400

    Adding Linode configuration (WIP)

--- terraform/modules/linode/kubernetes.tf
+++ terraform/modules/linode/kubernetes.tf
@@ -2,6 +2,7 @@ resource "linode_lke_cluster" "greenlight-development-cluster" {
   label       = var.cluster_name
   k8s_version = var.k8s_version
   region      = var.region
+  tags        = []
 
   pool {
     type  = var.machine_type
