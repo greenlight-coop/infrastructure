fd719a72675c9f40ac511500ce635d1a5f02ceab
Eric <etavela@gmail.com>
2021-10-31 06:55:32 -0400

    Adding Linode configuration (WIP)

---
+++ terraform/greenlight_linode/setup.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -ex
+
+terraform init
+
+# Install Linode cluster
+terraform apply -auto-approve -target=module.linode -target=null_resource.kubeconfig
+
+# Install Argo CD
+terraform apply -auto-approve -target=module.argo_cd
+kubectl -n argocd wait deployments -l app.kubernetes.io/part-of=argocd --for=condition=Available --timeout=240s
+
+# Install base cluster infrastructure
+terraform apply -auto-approve -target=module.project_cluster
+sleep 240
+kubectl -n istio-system wait deployments/istiod --for=condition=Available --timeout=600s
+sleep 120
+kubectl wait pods/loki-0 --for=condition=Ready --timeout=600s
+
+# Install development cluster infrastructure
+terraform apply -auto-approve -target=module.development_cluster_configuration
+terraform apply -auto-approve
