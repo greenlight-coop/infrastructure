6004c550dbd810610c90c4762060eb484d21521a
Eric <etavela@gmail.com>
2021-11-02 11:01:49 -0400

    Adding Linode configuration (WIP)

--- terraform/greenlight_linode/kubeconfig.tf
+++ terraform/greenlight_linode/kubeconfig.tf
@@ -2,15 +2,11 @@ resource "null_resource" "kubeconfig_get_output" {
   provisioner "local-exec" {
     command = "cat <<EOT > ~/.kube/${local.kubeconfig_output_filename} ${module.linode.kubeconfig}"
   }
-  provisioner "local-exec" {
-    when    = destroy
-    command = "rm ~/.kube/${local.kubeconfig_output_filename}"
-  }
 }
 
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
-    command = "ln -s ~/.kube/${local.kubeconfig_output_filename} ~/.kube/config"
+    command = "chmod 700 ln -s ~/.kube/${local.kubeconfig_output_filename} && ln -s ~/.kube/${local.kubeconfig_output_filename} ~/.kube/config"
   }
 
   depends_on = [
