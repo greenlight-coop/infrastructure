7b3dc8facc38a9c8587cbd449e5496796a999e59
Eric <etavela@gmail.com>
2021-11-03 21:22:39 -0400

    Revising setup scripts

--- terraform/greenlight_linode/kubeconfig.tf
+++ terraform/greenlight_linode/kubeconfig.tf
@@ -6,7 +6,11 @@ resource "null_resource" "kubeconfig_get_output" {
 
 resource "null_resource" "kubeconfig" {
   provisioner "local-exec" {
-    command = "chmod 700 ~/.kube/${local.kubeconfig_output_filename} && ln -s ~/.kube/${local.kubeconfig_output_filename} ~/.kube/config"
+    command = <<EOT
+      chmod 700 ~/.kube/${local.kubeconfig_output_filename} \
+        && rm -f ~/.kube/config \
+        && ln -s ~/.kube/${local.kubeconfig_output_filename} ~/.kube/config
+    EOT
   }
   provisioner "local-exec" {
     when    = destroy
