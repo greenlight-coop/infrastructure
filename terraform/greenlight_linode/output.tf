dd2771404a5977603b25d6b840e31d904f388682
Eric <etavela@gmail.com>
2021-11-04 06:42:12 -0400

    Adding Linode configuration (WIP)

--- terraform/greenlight_linode/output.tf
+++ terraform/greenlight_linode/output.tf
@@ -1,20 +1,3 @@
-# output "dns_zone_name" {
-#   value = module.google_project.dns_zone_name
-# }
-
-# output "cluster_endpoint" {
-#   value = module.google_project.cluster_endpoint
-# }
-
-# output "name_servers" {
-#   value = module.google_project.name_servers
-# }
-
-# output "cluster_ca_certificate" {
-#   value = module.google_project.cluster_ca_certificate
-#   sensitive = true
-# }
-
 output "admin_password" {
   value = local.admin_password
   sensitive = true
