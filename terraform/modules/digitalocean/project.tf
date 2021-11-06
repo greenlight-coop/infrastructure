f0c4ce5d846413d1aad5eebfc609d4fdf6de639a
Eric <etavela@gmail.com>
2021-11-06 06:45:50 -0400

    Adding DigitalOcean configuration (WIP)

--- terraform/modules/digitalocean/project.tf
+++ terraform/modules/digitalocean/project.tf
@@ -2,6 +2,6 @@ resource "digitalocean_project" "cluster_project" {
   name        = var.project_name
   resources   = [
     digitalocean_domain.app_domain.urn,
-    digitalocean_kubernetes_cluster.greenlight-development-cluster
+    digitalocean_kubernetes_cluster.greenlight-development-cluster.urn
   ]
 }
