42269dd43e0eb0488184bd48d39dec9dde5ae835
Eric <etavela@gmail.com>
2021-11-05 20:36:12 -0400

    Adding DigitalOcean configuration (WIP)

--- terraform/greenlight_digitalocean/main.tf
+++ terraform/greenlight_digitalocean/main.tf
@@ -35,9 +35,12 @@ terraform {
   backend "s3" {
     bucket = "tfstate-greenlight"
     key    = "tfstate"
-    region = "nyc3"
+    region = "us-east-1"
     endpoint = "nyc3.digitaloceanspaces.com"
+    skip_requesting_account_id = true
     skip_credentials_validation = true
+    skip_get_ec2_platforms = true
+    skip_metadata_api_check = true
   }
 }
 
