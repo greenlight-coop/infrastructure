f2108511da97e987810e0d26942f76886d39f2fc
Eric <etavela@gmail.com>
2021-11-05 20:39:19 -0400

    Adding DigitalOcean configuration (WIP)

--- terraform/modules/digitalocean/main.tf
+++ terraform/modules/digitalocean/main.tf
@@ -2,9 +2,9 @@ terraform {
   required_version = ">= 1.0.5"
 
   required_providers {
-    linode = {
-      source =  "linode/linode"
-      version = "~> 1.22.0"
+    digitalocean = {
+      source =  "digitalocean/digitalocean"
+      version = "~> 2.15.0"
     }
   }
 }
