083d6ac28af27e2ee926352587e961960e18e468
Eric <etavela@gmail.com>
2021-11-11 11:37:18 -0500

    Improving layout

--- terraform/modules/digitalocean/kubernetes.tf
+++ terraform/modules/digitalocean/kubernetes.tf
@@ -14,10 +14,10 @@ resource "digitalocean_kubernetes_cluster" "greenlight-development-cluster" {
   node_pool {
     name        = "primary-node-pool"
     size        = var.machine_type
-    auto_scale = true
-    min_nodes  = var.min_node_count
-    max_nodes  = var.max_node_count
-    tags = []
-    labels = {}
+    auto_scale  = true
+    min_nodes   = var.min_node_count
+    max_nodes   = var.max_node_count
+    tags        = []
+    labels      = {}
   }
 }
