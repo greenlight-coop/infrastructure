3a577307ce7fdaff5649d776e87e3f002ad80390
Eric <etavela@gmail.com>
2021-11-11 12:05:27 -0500

    Configured rook-ceph for use in cluster

--- terraform/modules/digitalocean/storageclass.tf
+++ terraform/modules/digitalocean/storageclass.tf
@@ -1,17 +1,3 @@
-resource "kubernetes_storage_class" "do-block-storage-waitforfirstconsumer" {
-  metadata {
-    name = "do-block-storage-waitforfirstconsumer"
-    labels = {
-      "c3.doks.digitalocean.com/component" = "csi-controller-service"
-      "c3.doks.digitalocean.com/plane" = "data"
-      "doks.digitalocean.com/managed" = "true"
-    }
-  }
-  storage_provisioner = "dobs.csi.digitalocean.com"
-  reclaim_policy      = "Delete"
-  volume_binding_mode = "WaitForFirstConsumer"
-}
-
 resource "null_resource" "remove_storage_class_change_default" {
   provisioner "local-exec" {
     command = <<EOT
